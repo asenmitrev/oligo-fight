f75e83d2ef03c4b8ae38ae5d506c8f4ee555fb52
Asen Mitrev <asen@writecraft.io>
2026-04-18 22:29:51 +0300

    STABLE AGAIN

--- scripts/character_db.gd
+++ scripts/character_db.gd
@@ -10,12 +10,13 @@ const _kraska_frames: SpriteFrames = preload("res://resources/kraska_sprite_fram
 var _by_display_name: Dictionary = {}
 var all_characters: Array = []
 
+var JUMP_VELOCITY = -3200 if OS.get_name() == "X11" else -1680
 
 func _init() -> void:
 	var kraska = _make_def("kraska", "Kraska", _kraska_frames, Color(1, 1, 1, 1))
 	kraska.punch_arm_extension = 225.0  # punch animation extends 20px further than Georgi's
 	kraska.speed = 350.0
-	kraska.jump_velocity = -1680.0
+	kraska.jump_velocity = JUMP_VELOCITY
 	kraska.punch_damage = 35
 	kraska.kick_damage = 9
 	kraska.block_damage_modifier = 0.15
@@ -27,7 +28,7 @@ func _init() -> void:
 	var simonka = _make_def("simonka", "Simonka", _simonka_frames, Color(1, 1, 1, 1))
 	simonka.punch_arm_extension = 140.0  # punch animation extends 20px further than Georgi's
 	simonka.speed = 380.0
-	simonka.jump_velocity = -2550.0
+	simonka.jump_velocity = JUMP_VELOCITY * 1.4
 	simonka.punch_damage = 12
 	simonka.kick_damage = 8
 	simonka.block_damage_modifier = 0.12
@@ -37,7 +38,7 @@ func _init() -> void:
 
 	var georgi = _make_def("georgi", "Georgi", _georgi_frames, Color(1, 1, 1, 1))
 	georgi.speed = 320.0
-	georgi.jump_velocity = -1600.0
+	georgi.jump_velocity = JUMP_VELOCITY
 	georgi.punch_damage = 15
 	georgi.kick_damage = 10
 	georgi.block_damage_modifier = 0.18
@@ -47,7 +48,7 @@ func _init() -> void:
 	var boekov = _make_def("boekov", "Georgiy", _boekov_frames, Color(1, 1, 1, 1))
 	boekov.sprite_offset = Vector2(0, 4)  # sprites sit ~7px higher than other chars
 	boekov.speed = 320.0
-	boekov.jump_velocity = -1600.0
+	boekov.jump_velocity = JUMP_VELOCITY
 	boekov.punch_damage = 15
 	boekov.kick_damage = 10
 	boekov.block_damage_modifier = 0.18
