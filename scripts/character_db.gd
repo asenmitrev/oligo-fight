d914057a375470af1e435a94aa43cf9b680180e2
Asen Mitrev <asen@writecraft.io>
2026-04-18 23:54:47 +0300

    Alex init

--- scripts/character_db.gd
+++ scripts/character_db.gd
@@ -4,7 +4,7 @@ const CharacterDef = preload("res://scripts/character_def.gd")
 const _simonka_frames: SpriteFrames = preload("res://resources/simonka_sprite_frames.tres")
 const _georgi_frames: SpriteFrames = preload("res://resources/georgi_sprite_frames.tres")
 const _bobe_frames: SpriteFrames = preload("res://resources/bobe_sprite_frames.tres")
-const _kraska_frames: SpriteFrames = preload("res://resources/kraska_sprite_frames.tres")
+const _ipman_frames: SpriteFrames = preload("res://resources/ipman_sprite_frames.tres")
 
 var _by_display_name: Dictionary = {}
 var all_characters: Array = []
@@ -12,17 +12,17 @@ var all_characters: Array = []
 var JUMP_VELOCITY = -3200 if OS.get_name() == "X11" else -1680
 
 func _init() -> void:
-	var kraska = _make_def("kraska", "Kraska", _kraska_frames, Color(1, 1, 1, 1))
-	kraska.punch_arm_extension = 225.0  # punch animation extends 20px further than Georgi's
-	kraska.speed = 350.0
-	kraska.jump_velocity = JUMP_VELOCITY
-	kraska.punch_damage = 35
-	kraska.kick_damage = 9
-	kraska.block_damage_modifier = 0.15
-	kraska.max_health = 110
-	kraska.body_punch_enabled = true
-	kraska.kick_speed_scale = 1.3
-	_register(kraska)
+	var ipman = _make_def("ipman", "IpMan", _ipman_frames, Color(1, 1, 1, 1))
+	ipman.punch_arm_extension = 200.0
+	ipman.speed = 360.0
+	ipman.jump_velocity = JUMP_VELOCITY
+	ipman.punch_damage = 30
+	ipman.kick_damage = 9
+	ipman.block_damage_modifier = 0.12
+	ipman.max_health = 105
+	ipman.body_punch_enabled = false
+	ipman.kick_speed_scale = 1.2
+	_register(ipman)
 
 	var simonka = _make_def("simonka", "Simonka", _simonka_frames, Color(1, 1, 1, 1))
 	simonka.punch_arm_extension = 140.0  # punch animation extends 20px further than Georgi's
